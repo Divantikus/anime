817a48cfbd4279606ef1672f5f990d8a7a4e6c3c
Divantikus <149273212+Divantikus@users.noreply.github.com>
2024-07-23 14:19:30 +0300

    minor edit before deployment

--- src/components/select/CustomSelect.tsx
+++ src/components/select/CustomSelect.tsx
@@ -1,34 +1,40 @@
-import {Controller, useFormContext} from "react-hook-form";
-import {Options} from "src/components/search-form-by-parameters/SearchFormTypes.ts";
-import Select, {StylesConfig} from "react-select";
-import {FC} from "react";
+import { Controller, useFormContext } from 'react-hook-form';
+import Select, { StylesConfig } from 'react-select';
+import { Options } from 'src/components/search-form-by-parameters/SearchFormTypes.ts';
+import { FC } from 'react';
 
-interface CustomSelectProps{
-    placeholder: string,
-    options: Readonly<Options[]>,
-    styles: StylesConfig,
-    selName: string
+interface CustomSelectProps {
+    placeholder: string;
+    options: Readonly<Options[]>;
+    styles: StylesConfig;
+    selName: string;
 }
 
-export const CustomSelect: FC<CustomSelectProps> = ({placeholder, options, styles, selName}) => {
-
-    const {control} = useFormContext()
+export const CustomSelect: FC<CustomSelectProps> = ({
+    placeholder,
+    options,
+    styles,
+    selName,
+}) => {
+    const { control } = useFormContext();
 
     return (
         <Controller
             control={control}
-            render={({field: {onChange, onBlur, value}}) => {
-                return <Select
-                    isMulti={true}
-                    onBlur={onBlur}
-                    styles={styles}
-                    selected={value}
-                    options={options}
-                    onChange={onChange}
-                    placeholder={placeholder}
-                />
+            render={({ field: { onChange, onBlur, value } }) => {
+                return (
+                    <Select
+                        isMulti={true}
+                        onBlur={onBlur}
+                        styles={styles}
+                        value={value}
+                        options={options}
+                        onChange={onChange}
+                        placeholder={placeholder}
+                    />
+                );
             }}
             name={selName}
         />
-    )
-}
+    );
+};
