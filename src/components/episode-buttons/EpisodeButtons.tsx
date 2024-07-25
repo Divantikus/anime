9583c826baafd47f3a69e3d7e72cd402c831f1b1
Divantikus <149273212+Divantikus@users.noreply.github.com>
2024-07-25 15:38:45 +0300

    fixed a bug with the series drop-down list (ts- ignoring is present)

--- src/components/episode-buttons/EpisodeButtons.tsx
+++ src/components/episode-buttons/EpisodeButtons.tsx
@@ -1,3 +1,4 @@
+import { FC, useEffect, useRef } from 'react';
 import { selectEpisodeStyles } from 'src/components/episode-buttons/selectEpisodeStyles.ts';
 import Select, { SingleValue } from 'react-select';
 import { EpisodeButtonsProps } from 'src/components/episode-buttons/EpisodeButtonsTypes.ts';
@@ -5,13 +6,23 @@ import {
     createEpisodeButton,
     SelectOption,
 } from 'src/functions/createEpisodeButton.ts';
-import { FC } from 'react';
 
 export const EpisodeButtons: FC<EpisodeButtonsProps> = ({
     mutate,
     id,
     data,
 }) => {
+    const selectRef = useRef(null);
+
+    useEffect(() => {
+        const clearSelect = () => {
+            if (!selectRef.current) return;
+            //@ts-ignore
+            selectRef.current.popValue();
+        };
+        clearSelect();
+    }, [data]);
+
     const changeVideo = (event: unknown) => {
         const changeVideoEvent = event as SingleValue<SelectOption>;
 
@@ -22,9 +33,11 @@ export const EpisodeButtons: FC<EpisodeButtonsProps> = ({
 
     return (
         <Select
+            ref={selectRef}
             onChange={changeVideo}
             styles={selectEpisodeStyles}
             placeholder={'Эпизод не выбран'}
+            noOptionsMessage={() => 'Серии отсутствуют'}
             defaultValue={`Серия ${data.episode_number}`}
             options={createEpisodeButton(data.episodes_number)}
         />
