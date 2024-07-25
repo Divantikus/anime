6af440c78b63fc975f1892335abda5b42d7c3793
Divantikus <149273212+Divantikus@users.noreply.github.com>
2024-07-25 13:23:34 +0300

    adding a message output in the absence of anime in releases

--- src/components/pages/Releases.tsx
+++ src/components/pages/Releases.tsx
@@ -1,19 +1,12 @@
 import { PageSelectionButtons } from 'src/components/page-selection-buttons/PageSelectionButtons.tsx';
 import { useMutationReleases } from 'src/hooks/useMutationReleases.ts';
 import { useEffect, useState } from 'react';
+import { releaseCardStyles } from 'src/components/pages/variables/ReleasesVariables.ts';
 import { SearchForm } from 'src/components/search-form-by-parameters/SearchForm.tsx';
 import { Loading } from 'src/components/loading/Loading.tsx';
 import { Card } from 'src/components/Card/Card.tsx';
 import { Link } from 'react-router-dom';
 import style from './styles/Releases.module.scss';
-
-const styles = {
-    height: 390,
-    border: 0,
-    fontSize: { title: 14, episode: 14, description: 12 },
-    width: 270,
-    margin: 0,
-};
 
 export const Releases = () => {
     const { mutate, data, isLoading, isError } = useMutationReleases();
@@ -33,25 +26,37 @@ export const Releases = () => {
             {isLoading && <Loading height={20} />}
             {data && (
                 <>
-                    <div className={style.titles}>
-                        {data.anime_list.map((title) => {
-                            return (
-                                <Link
-                                    to={`/watch/${title.slug}`}
-                                    key={title.id}
-                                    className={style.link}
-                                    onClick={() => window.scroll(0, 0)}
-                                >
-                                    <Card styles={styles} info={title} />
-                                </Link>
-                            );
-                        })}
-                    </div>
-                    <PageSelectionButtons
-                        currentPage={currentPage}
-                        setCurrentPage={setCurrentPage}
-                        data={data}
-                    />
+                    {!data.anime_list.length && (
+                        <h2 className={style.animeIsMissingTitle}>
+                            Нет подходящих аниме
+                        </h2>
+                    )}
+                    {!!data.anime_list.length && (
+                        <>
+                            <div className={style.titles}>
+                                {data.anime_list.map((title) => {
+                                    return (
+                                        <Link
+                                            to={`/watch/${title.slug}`}
+                                            key={title.id}
+                                            className={style.link}
+                                            onClick={() => window.scroll(0, 0)}
+                                        >
+                                            <Card
+                                                styles={releaseCardStyles}
+                                                info={title}
+                                            />
+                                        </Link>
+                                    );
+                                })}
+                            </div>
+                            <PageSelectionButtons
+                                currentPage={currentPage}
+                                setCurrentPage={setCurrentPage}
+                                data={data}
+                            />
+                        </>
+                    )}
                 </>
             )}
             {isError && <div>Error (</div>}
---
+++ src/components/pages/variables/ReleasesVariables.ts
@@ -0,0 +1,7 @@
+export const releaseCardStyles = {
+    height: 390,
+    border: 0,
+    fontSize: { title: 14, episode: 14, description: 12 },
+    width: 270,
+    margin: 0,
+};
